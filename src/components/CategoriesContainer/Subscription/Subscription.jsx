53db1da1b1322c7a8698f3b42ed6afd467f4ae43
Anup Narvekar <narvekaranup321@gmail.com>
2024-08-13 21:05:39 +0530

    fixed icon bug

--- src/components/CategoriesContainer/Subscription/Subscription.jsx
+++ src/components/CategoriesContainer/Subscription/Subscription.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './Subscription.css'
 
-const SubscriptionCard = ({title, unreadCount, iconName}) => {
+const SubscriptionCard = ({ title, unreadCount, iconName }) => {
+
+    let [isSelected, setIsSelected] = useState(false);
+
+    const handleClick = () => {
+        setIsSelected(!isSelected);
+    }
+
     return (
-        <div className="subscriptionContainer">
+        <div className={isSelected ? "subscriptionContainer selected" : "subscriptionContainer"} onClick={handleClick}>
             <span className="material-symbols-rounded folderIcon">
-                {iconName}
+                {(iconName == 'folder' && (isSelected ? 'folder_open' : 'folder'))}
+                {(iconName != 'folder' && iconName)}
             </span>
             <p className='subscriptionTitle'>{title}</p>
             <p className='subscriptionUnreadCount'>{unreadCount}</p>
